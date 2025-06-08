df4176b14b46eb51fbbe65b81a72464d8e6dfa17
VijayCS <134143862+Vijaygowda09@users.noreply.github.com>
2025-06-08 18:44:17 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,3 +8,23 @@ from .p7 import p7
 from .p8 import p8
 from .p9 import p9
 from .p10 import p10
+p1='''import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+df = fetch_california_housing(as_frame=True).frame
+nf = df.select_dtypes(include=[np.number]).columns
+for col in nf:
+     sns.histplot(df[col], kde=True)
+     plt.title(col)
+     plt.show()
+     sns.boxplot(x=df[col])
+     plt.title(col)
+     plt.show()
+for col in nf:
+      Q1, Q3 = df[col].quantile([0.25, 0.75])
+      IQR = Q3 - Q1
+      outliers = df[(df[col] < Q1 - 1.5 * IQR) | (df[col] > Q3 + 1.5 * IQR)]
+      print(f"{col}: {len(outliers)} outliers")
+print(df.describe())'''
